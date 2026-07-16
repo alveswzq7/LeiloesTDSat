fdf146edee3cc7f082f7e31e6aa81272a5296864
alveswzq7 <vanildojunior311008@gmail.com>
2026-07-16 19:37:17 -0300

    implementa venda e consulta de produtos vendidos

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -78,8 +78,61 @@ public class ProdutosDAO {
 
     return listagem;
 }
-    
-    
+    public boolean venderProduto(int id){
+
+    conn = new conectaDAO().connectDB();
+
+    try {
+
+        String sql = "UPDATE produtos SET status = 'Vendido' WHERE id = ?";
+
+        prep = conn.prepareStatement(sql);
+
+        prep.setInt(1, id);
+
+        prep.executeUpdate();
+
+        return true;
+
+    } catch (Exception e) {
+
+        JOptionPane.showMessageDialog(null, "Erro ao vender: " + e.getMessage());
+        return false;
+
+    }
+
+}
+    public ArrayList<ProdutosDTO> listarProdutosVendidos(){
+
+    conn = new conectaDAO().connectDB();
+
+    ArrayList<ProdutosDTO> listagem = new ArrayList<>();
+
+    String sql = "SELECT * FROM produtos WHERE status = 'Vendido'";
+
+    try {
+
+        prep = conn.prepareStatement(sql);
+        resultset = prep.executeQuery();
+
+        while(resultset.next()){
+
+            ProdutosDTO produto = new ProdutosDTO();
+
+            produto.setId(resultset.getInt("id"));
+            produto.setNome(resultset.getString("nome"));
+            produto.setValor(resultset.getInt("valor"));
+            produto.setStatus(resultset.getString("status"));
+
+            listagem.add(produto);
+        }
+
+    } catch (Exception e) {
+        JOptionPane.showMessageDialog(null, "Erro ao listar vendidos: " + e.getMessage());
+    }
+
+    return listagem;
+}
     
         
 }
